bc6aac0a736760f3d6e87b8a44c14a8259573bc0
souravsingha13 <souravsingha@uatech.biz>
2022-02-14 18:08:01 +0600

    hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-import ClickCount from "./components/contextAPI/ClickCount";
-import Counter from "./components/contextAPI/Counter";
-import Section from "./components/contextAPI/section";
-import ThemeContext from "./components/contextAPI/themeContext";
-
+import HookState from "./components/hook/Hookstate";
 
 
 export default class App extends React.Component {
@@ -14,12 +10,7 @@ export default class App extends React.Component {
     const { theme } = this.state;
     return (  
       <div className="app">
-        <Counter>
-          {(count,increamentCount)=>{
-            <ClickCount count={count} increamentCount={increamentCount}/>
-          } }
-        </Counter>
-        <ThemeContext.Provider value={{theme : theme}}><Section/></ThemeContext.Provider>
+        <HookState/>
       </div>
     );
   }
